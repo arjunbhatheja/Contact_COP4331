77b6262d440ea2287300c22d815b3a096d560002
Arjun Bhatheja <93717249+arjunbhatheja@users.noreply.github.com>
2024-08-28 18:23:45 -0400

    Add files via upload

---
+++ LAMP Stack/LAMP Stack/LAMPAPI/Register.php
@@ -0,0 +1,60 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$smp = $conn-> prepare("Select * from Users WHERE Login = ? and Password = ?");
+		$smp->bind_param("s", $login);
+		$smp->execute();
+		$result = $smp->get_result();
+		$rows = mysqli_num_rows($result);
+		if ($rows == 0)
+		{
+			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+			$stmt->execute();
+			$id = $conn->insert_id;
+			$stmt->close();
+			$conn->close();
+			$searchResults .= '{'.'"id": "'.$id.''.'"}';
+
+			returnWithInfo($searchResults);
+		} else {
+			http_response_code(409);
+			returnWithError("Username taken");
+		}
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
---
+++ LAMP Stack/LAMP Stack/LAMPAPI/UpdateContacts.php
@@ -0,0 +1,44 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName=?, PhoneNumber= ?, EmailAddress= ? WHERE ID= ?");
+		$stmt->bind_param("ssisi", $inData["newFirstName"], $inData["newLastName"], $inData["newPhoneNumber"], $inData["newEmailAddress"], inData["newID"]);
+		$stmt->execute();
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
