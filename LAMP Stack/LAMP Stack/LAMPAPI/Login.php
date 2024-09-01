1b09d65c93e6b02702f2d3e841a712da4601f9a9
Santiago Echeverry <93231171+Saltyago@users.noreply.github.com>
2024-09-01 18:11:41 -0400

    Update Login.php
    Moving things back so it works on the website. I did not realize it was the javascript file that was reading the info.

--- LAMP Stack/LAMP Stack/LAMPAPI/Login.php
+++ LAMP Stack/LAMP Stack/LAMPAPI/Login.php
@@ -15,7 +15,7 @@
 	else
 	{
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
