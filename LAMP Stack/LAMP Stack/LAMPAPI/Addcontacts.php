318689ff3df340712584df8c8e9f113790c515ab
Santiago Echeverry <93231171+Saltyago@users.noreply.github.com>
2024-09-30 17:44:10 -0400

    Update Addcontacts.php
    Final Version

--- LAMP Stack/LAMP Stack/LAMPAPI/Addcontacts.php
+++ LAMP Stack/LAMP Stack/LAMPAPI/Addcontacts.php
@@ -1,6 +1,6 @@
 <?php
 	$inData = getRequestInfo();
-	
+
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
 	{
@@ -8,13 +8,50 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("issss", $inData["userID"], $inData["firstName"],$inData["lastName"],$inData["phone"],$inData["email"]);
+		if((strlen(trim($inData["firstName"])) == 0)||(strlen(trim($inData["lastName"])) == 0)||(strlen(trim($inData["phone"])) == 0)||(strlen(trim($inData["email"])) == 0)){
+			http_response_code(400);
+      returnWithInfo("Fill in information");
+			exit();
+		}
+		
 
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
+		$mon = $conn->prepare("Select * from Users WHERE ID = ?");
+		$mon->bind_param("i", $inData["userId"]);
+		$mon->execute();
+		$res = $mon->get_result();
+			
+		$userrow = mysqli_num_rows($res);
+
+		if($userrow != 0){
+			if(!empty($inData["firstName"])){
+			$smp = $conn->prepare("Select * from Contacts WHERE FirstName = ? and LastName = ? and Phone = ?");
+			$smp->bind_param("sss", $inData["firstName"], $inData["lastName"],$inData["phone"]);
+			$smp->execute();
+			$result = $smp->get_result();
+			$rows = mysqli_num_rows($result);
+			if($rows == 0){
+				$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
+				$stmt->bind_param("ssssi", $inData["firstName"], $inData["lastName"], $inData["phone"], $inData["email"],$inData["userId"]);
+				$stmt->execute();
+				$stmt->close();
+				$smp->close();
+				$mon->close();
+				$conn->close();
+				}
+			else{
+				http_response_code(409);
+				returnWithError("Contact already exists");
+				}
+			}
+			else{
+				http_response_code(400);
+			returnWithError("Name required");
+			}
+		}
+		 else{
+			http_response_code(406);
+			returnWithError("UserId is invalid");
+		}
 	}
 
 	function getRequestInfo()
