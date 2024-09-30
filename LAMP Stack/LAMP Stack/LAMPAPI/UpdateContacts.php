3931d16093e9ccbab93b648af0d965b95c444e5f
Santiago Echeverry <93231171+Saltyago@users.noreply.github.com>
2024-09-30 17:41:11 -0400

    Update UpdateContacts.php
    Final Version

--- LAMP Stack/LAMP Stack/LAMPAPI/UpdateContacts.php
+++ LAMP Stack/LAMP Stack/LAMPAPI/UpdateContacts.php
@@ -12,10 +12,23 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName=?, PhoneNumber= ?, EmailAddress= ? WHERE ID= ?");
-		$stmt->bind_param("ssisi", $inData["newFirstName"], $inData["newLastName"], $inData["newPhoneNumber"], $inData["newEmailAddress"], inData["newID"]);
+		if((strlen(trim($inData["firstName"])) == 0)||(strlen(trim($inData["lastName"])) == 0)||(strlen(trim($inData["phone"])) == 0)||(strlen(trim($inData["email"])) == 0)){
+			http_response_code(400);
+      returnWithInfo("Fill in information");
+			exit();
+		}
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?,  LastName =?,  Phone = ?,  Email = ? WHERE ID = ? AND UserID = ?");
+		$stmt->bind_param("ssssii",$inData["firstName"],$inData["lastName"],$inData["phone"],$inData["email"], $inData["id"],$inData["userId"]);
 		$stmt->execute();
-
+		$affectedRows = $stmt->affected_rows;
+    
+    		if ($affectedRows > 0) {
+        		http_response_code(200);
+        		returnWithInfo("Contact updated successfully");
+    		} else {
+       			http_response_code(400);
+        		returnWithError("Update failed: No matching contact found with the given  Id");
+    		}
 		$stmt->close();
 		$conn->close();
 	}
